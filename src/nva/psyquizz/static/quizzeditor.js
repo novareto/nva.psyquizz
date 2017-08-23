9c7fcb3ad06dfcdde2a601d76185868f70131324
Souheil CHELFOUH <trollfot@gmail.com>
2017-08-23 16:36:29 +0200

    Customized richtext editor

--- src/nva/psyquizz/static/quizzeditor.js
+++ src/nva/psyquizz/static/quizzeditor.js
@@ -1,5 +1,25 @@
 $( document ).ready(function() {
-    $('#form-field-about').summernote({height: 300});
-    $('#form-field-text').summernote({height: 300});
+    $('form[id!="CreateCourse"] #form-field-about').summernote({
+        height: 300,
+        toolbar: [
+            ['style', ['bold', 'underline']],
+        ]
+    });
+
+    $('form[id!="CreateCourse"] #form-field-text').summernote({
+        height: 300,
+        toolbar: [
+            ['style', ['bold', 'underline']],
+        ]
+    });
+
+    $('form#CreateCourse #form-field-about').summernote({
+        height: 300,
+    });
+
+    $('form#CreateCourse #form-field-text').summernote({
+        height: 300,
+    });
+
     $('#form-field-form-field-criterias :input').prop('checked', 'true')
 });
